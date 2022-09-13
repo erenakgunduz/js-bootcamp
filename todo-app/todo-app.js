dac412b2697c3917dbbba4a9d0d80073ca8860f5
erenakgunduz <coderenius@gmail.com>
2022-09-13 16:49:49 -0400

    Refactored todos app

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -7,68 +7,11 @@
 //   }
 // });
 
-let todos = [];
+const todos = getSavedTodos();
 
 const filters = {
   searchText: "",
   hideCompleted: document.getElementById("hide-completed"),
-};
-
-// Check for existing saved data
-const todosJSON = localStorage.getItem("todos");
-if (todosJSON !== null) {
-  todos = JSON.parse(todosJSON);
-}
-
-// Filters and renders list
-const renderTodos = (todos, filters) => {
-  const filteredTodos = (list) => {
-    return list.filter((item) => {
-      return item.text.toLowerCase().includes(filters.searchText.toLowerCase());
-    });
-  };
-
-  const getPendingTodos = todos.filter((todo) => {
-    return !todo.completed;
-  });
-
-  document.querySelector("header").innerHTML = "<h1>Todo App</h1>";
-
-  const pendingTodos = document.createElement("h2");
-  if (filteredTodos(getPendingTodos).length === 1) {
-    pendingTodos.textContent = `You have one todo left`;
-  } else {
-    pendingTodos.textContent = `You have ${
-      filteredTodos(getPendingTodos).length
-    } todos left`;
-  }
-  document.querySelector("header").appendChild(pendingTodos);
-
-  const renderedTodos = (render) => {
-    document.querySelector(".todos .todo-list").innerHTML = "";
-    render.forEach((todo) => {
-      const toDoList = document.createElement("li");
-      toDoList.textContent = todo.text;
-      document.querySelector(".todos .todo-list").appendChild(toDoList);
-    });
-  };
-
-  // Render based on current state of checkbox at time of function call
-  // Which is every keystroke of search box due to event listener below
-  if (filters.hideCompleted.checked) {
-    renderedTodos(filteredTodos(getPendingTodos));
-  } else {
-    renderedTodos(filteredTodos(todos));
-  }
-
-  // Also need event listener here to respond to every change to checkbox state
-  document.querySelector("#hide-completed").addEventListener("change", (e) => {
-    if (e.target.checked) {
-      return renderedTodos(filteredTodos(getPendingTodos));
-    } else {
-      return renderedTodos(filteredTodos(todos));
-    }
-  });
 };
 
 renderTodos(todos, filters);
@@ -82,15 +25,8 @@ document.querySelector("#search-text").addEventListener("input", (e) => {
 // Listens for form submission and appends new todo to list
 document.querySelector("#new-todo").addEventListener("submit", (e) => {
   e.preventDefault();
-
-  if (e.target.elements.newTodoText.value.trim().length > 0) {
-    todos.push({
-      text: e.target.elements.newTodoText.value.trim(),
-      completed: false,
-    });
-  }
-  localStorage.setItem("todos", JSON.stringify(todos));
-
+  pushTodos(todos, e);
+  saveTodos(todos);
   renderTodos(todos, filters);
   return (e.target.elements.newTodoText.value = "");
 });
---
+++ todo-app/todo-functions.js
@@ -0,0 +1,78 @@
+// Fetch existing todos from localStorage
+const getSavedTodos = () => {
+  const todosJSON = localStorage.getItem("todos");
+  if (todosJSON !== null) {
+    return JSON.parse(todosJSON);
+  } else {
+    return [];
+  }
+};
+
+// Append a new incomplete todo item
+const pushTodos = (todos, e) => {
+  if (e.target.elements.newTodoText.value.trim().length > 0) {
+    todos.push({
+      text: e.target.elements.newTodoText.value.trim(),
+      completed: false,
+    });
+  }
+};
+
+// Save todos to localStorage
+const saveTodos = (todos) => {
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+// Display summary
+const getSummary = (todos) => {
+  const pendingTodos = document.createElement("h2");
+  if (todos.length === 1) {
+    pendingTodos.textContent = "You have one todo left";
+  } else {
+    pendingTodos.textContent = `You have ${todos.length} todos left`;
+  }
+  document.querySelector("header").appendChild(pendingTodos);
+};
+
+// Generate DOM render
+const generateTodoDOM = (render) => {
+  document.querySelector(".todos .todo-list").innerHTML = "";
+  render.forEach((todo) => {
+    const toDoList = document.createElement("li");
+    toDoList.textContent = todo.text;
+    document.querySelector(".todos .todo-list").appendChild(toDoList);
+  });
+};
+
+// Render application todos based on filters
+const renderTodos = (todos, filters) => {
+  const filteredTodos = (list) => {
+    return list.filter((item) => {
+      return item.text.toLowerCase().includes(filters.searchText.toLowerCase());
+    });
+  };
+
+  const getPendingTodos = todos.filter((todo) => {
+    return !todo.completed;
+  });
+
+  document.querySelector("header").innerHTML = "<h1>Todo App</h1>";
+  getSummary(filteredTodos(getPendingTodos));
+
+  // Render based on current state of checkbox at time of function call
+  // Which is every keystroke of search box due to event listener
+  if (filters.hideCompleted.checked) {
+    generateTodoDOM(filteredTodos(getPendingTodos));
+  } else {
+    generateTodoDOM(filteredTodos(todos));
+  }
+
+  // Also need event listener here to respond to every change to checkbox state
+  document.querySelector("#hide-completed").addEventListener("change", (e) => {
+    if (e.target.checked) {
+      return generateTodoDOM(filteredTodos(getPendingTodos));
+    } else {
+      return generateTodoDOM(filteredTodos(todos));
+    }
+  });
+};
