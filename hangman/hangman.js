f1be35acfb295e108427765de85eabdeb9d00eda
erenakgunduz <coderenius@gmail.com>
2022-09-30 14:30:19 -0400

    Prepare hangman (again)

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -91,6 +91,7 @@ Hangman.prototype.makeGuess = function (guess) {
 
 const game1 = new Hangman('Cat', 2);
 
+console.log(game1.getPuzzle());
 window.addEventListener('keypress', (e) => {
   const guess = e.key;
   game1.makeGuess(guess);
